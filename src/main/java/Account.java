92f8209e96703867f9e27e756bbe1e9f61ec9f34
dlrudfh <rudfh5256@naver.com>
2021-04-29 22:13:49 +0900

    TTD실습

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,2 +1,20 @@
 public class Account {
+    private int balance;
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public void deposit(int money) {
+        this.balance += money;
+    }
+
+    public int withdraw(int money) {
+        if (money > this.balance)
+            return this.balance;
+        else {
+            this.balance -= money;
+            return this.balance;
+        }
+    }
 }
